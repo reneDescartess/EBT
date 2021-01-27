73bb92506815e169ac33f132861dbafa67946f65
Tony Luo <tonyluozn@gmail.com>
2021-01-27 20:00:21 +0800

    Conversion Interface update

--- app/src/components/ConvertPanel.js
+++ app/src/components/ConvertPanel.js
@@ -5,9 +5,26 @@ import './ConvertPanel.css';
 function ConvertPanel(props){
     const [balance,setBalance] = useState(0);
     const [amount, setAmount] = useState(0);
+    const [token, setToken] = useState(0);
 
     return (
-        <div></div>
+        <div className='wrapper'>
+            <div className='left'>
+                <p className='upper-text'>Balance: {balance}</p>
+                <p className='amount'>{amount}</p>
+            </div>
+            <div className='right'>
+                <p className='upper-text'>From</p>
+                <div className='token-selection'>
+                    <div className='token-name'>
+                        <p>ETH</p>
+                    </div>
+                    <div className='dropdown'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
