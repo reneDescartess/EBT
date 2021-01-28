1d7842381dbcfd3d1283c3254110a2908138f555
Danny Luo <zeping.luo@duke.edu>
2021-01-28 14:29:47 +0800

    exchange

--- app/src/components/ConvertPanel.js
+++ app/src/components/ConvertPanel.js
@@ -3,35 +3,67 @@ import 'antd/dist/antd.css';
 import './ConvertPanel.css';
 import {DownOutlined} from '@ant-design/icons';
 function ConvertPanel(props){
-    const [balance,setBalance] = useState(0);
-    const [amount, setAmount] = useState(0);
-    const [token, setToken] = useState("ETH");
+    function precise(x) {
+        return Number.parseFloat(x).toPrecision(4);
+      }
+      
+    
 
     return (
-        <div className='wrapper'>
+        <div>
+            <div className='wrapper'>
+                <div className='left'>
+                    <p className='upper-text'>Balance: {props.input}</p>
+                    {/* <p className='amount'>{amount}</p> */}
+                    <input type="number" id="From" 
+                           minlength="4" maxlength="8" size="10" defaultValue={props.input}
+                           onChange={e=>props.setInput(e.target.value)}></input>
+                   
+                </div>
+                <div className='right'>
+                    <p className='upper-text'>From</p>
+                
+                    <div class="select-container">
+                        <select onChange={(e)=>props.setFrom(e.target.value)}>
+                            <option value="none" class="option">Select</option>
+                            <option value="EBT" class="option">EBT</option>
+                            <option value="FRAX" class="option">FRAX</option>
+                            <option value="ETH" class="option">ETH</option>
+                        </select>
+                        <div class="down-arrow"> 
+                            <DownOutlined style={{fontSize:"15px",color:"black"}} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='wrapper'>
             <div className='left'>
-                <p className='upper-text'>Balance: {balance}</p>
-                <p className='amount'>{amount}</p>
+                <p className='upper-text'>Balance:{props.exchangeRate==0?"NA":precise(props.input/props.exchangeRate)} </p>
+                <p className='amount'>{props.exchangeRate==0?"NA":precise(props.input/props.exchangeRate)}</p>
             </div>
             <div className='right'>
-                <p className='upper-text'>From</p>
+                <p className='upper-text'>To</p>
                 {/* <button className='token-selection'>
                     <p className='token-name'> {token} </p>
                 </button> */}
                 <div class="select-container">
-                    <select>
-                        <option value="1" class="option">1</option>
-                        <option value="2" class="option">2</option>
-                        <option value="3" class="option">3</option>
-                        <option value="4" class="option">4</option>
-                        <option value="5" class="option">5</option>
+                    <select onChange={(e)=>props.setTo(e.target.value)}>
+                        <option value="none" class="option">Select</option>
+                        <option value="EBT" class="option">EBT</option>
+                        <option value="FRAX" class="option">FRAX</option>
+                        <option value="ETH" class="option">ETH</option>
                     </select>
                     <div class="down-arrow"> 
                         <DownOutlined style={{fontSize:"15px",color:"black"}} />
                     </div>
                 </div>
             </div>
+            </div>
+
+
         </div>
+        
     )
 
 }
--- app/src/pages/ExchangePage.js
+++ app/src/pages/ExchangePage.js
@@ -1,5 +1,5 @@
 import { newContextComponents } from "@drizzle/react-components";
-import React, {useContext, useEffect,useState } from 'react'
+import React, {useContext, useEffect,useState, Fragment } from 'react'
 import 'antd/dist/antd.css';
 import NavBar from '../components/NavBar';
 import ConvertPanel from '../components/ConvertPanel';
@@ -8,22 +8,66 @@ import './ExchangePage.css';
 const { AccountData, ContractData, ContractForm } = newContextComponents;
 
 const ExchangePage =({ drizzle, drizzleState }) => {
+    function precise(x) {
+        return Number.parseFloat(x).toPrecision(4);
+      }
+      
     // destructure drizzle and drizzleState from props
-    const [input, setInput] = useState(null);
-    const [exchangeRate, setExchangeRate] = useState(2333); 
-    const [from, setFrom] = useState("FRAX"); 
-    const [to, setTo] = useState("ETH"); 
+    const EBT_USD=1;
+    const FRAX_USD=0.989;
+    const ETH_USD=1296.3;
+    const [input, setInput] = useState(0);
+    const [exchangeRate, setExchangeRate] = useState(0); 
+    const [from, setFrom] = useState("none"); 
+    const [to, setTo] = useState("none"); 
+    useEffect(()=>{
+        switch(from + to){
+            case "none"+"none":
+                setExchangeRate(0)
+                break;
+            case "EBT"+"FRAX":
+                setExchangeRate(FRAX_USD/EBT_USD)
+                break;
+            case "EBT"+"ETH":
+                setExchangeRate(ETH_USD/EBT_USD)
+                break;
+            case "FRAX"+"EBT":
+                setExchangeRate(EBT_USD/FRAX_USD)
+                break;
+            case "FRAX"+"ETH":
+                setExchangeRate(ETH_USD/FRAX_USD)
+                break;
+            case "ETH"+"FRAX":
+                setExchangeRate(FRAX_USD/ETH_USD)
+                break;
+            case "ETH"+"EBT":
+                setExchangeRate(EBT_USD/ETH_USD)
+                break;
+            case "ETH"+"ETH" :
+                setExchangeRate(1)
+                break;
+            case "EBT"+"EBT" :
+                setExchangeRate(1)
+                break;
+            case "FRAX"+"FRAX":
+                setExchangeRate(1)
+                break;
+        }
+
+    },[from,to]);
     return (
         <div>
             <NavBar/>
             <div className='form-wrapper'>
-                <ConvertPanel/>
-                <ConvertPanel/>
+                <ConvertPanel input={input} setInput={setInput} exchangeRate={exchangeRate} setExchangeRate={setExchangeRate}
+                from={from} setFrom={setFrom} to={to} setTo={setTo}/>
+               
                 <div className='price-panel'>
                     <p className='left-text'>Price</p>
-                    <p className='right-text'>{exchangeRate} {from} per {to}</p>
+                    <p className='right-text'>{precise(exchangeRate)} {from} per {to}</p>
                 </div>
-                <button className='submit'>Swap</button>
+                {/* <button className='submit'>Swap</button> */}
+                <a class="swap-button">Swap</a>
 
             </div>
 
