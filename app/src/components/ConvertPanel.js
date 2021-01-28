4311ac6e15e271ed290c137f41a2e5cdc27ed8f0
Danny Luo <zeping.luo@duke.edu>
2021-01-28 13:12:59 +0800

    drop down

--- app/src/components/ConvertPanel.js
+++ app/src/components/ConvertPanel.js
@@ -1,7 +1,7 @@
 import React , { useState } from 'react';
 import 'antd/dist/antd.css';
 import './ConvertPanel.css';
-
+import {DownOutlined} from '@ant-design/icons';
 function ConvertPanel(props){
     const [balance,setBalance] = useState(0);
     const [amount, setAmount] = useState(0);
@@ -15,9 +15,21 @@ function ConvertPanel(props){
             </div>
             <div className='right'>
                 <p className='upper-text'>From</p>
-                <button className='token-selection'>
+                {/* <button className='token-selection'>
                     <p className='token-name'> {token} </p>
-                </button>
+                </button> */}
+                <div class="select-container">
+                    <select>
+                        <option value="1" class="option">1</option>
+                        <option value="2" class="option">2</option>
+                        <option value="3" class="option">3</option>
+                        <option value="4" class="option">4</option>
+                        <option value="5" class="option">5</option>
+                    </select>
+                    <div class="down-arrow"> 
+                        <DownOutlined style={{fontSize:"15px",color:"black"}} />
+                    </div>
+                </div>
             </div>
         </div>
     )
